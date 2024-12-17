2353da0cd5a9639b929214a645ef0781ed59c7b7
brunomarques <brunomdsv5@gmail.com>
2024-12-17 18:03:57 -0300

    adicionado exercicio de dicionarios

---
+++ programe.py-ufpe/5-dicionarios/ex1.py
@@ -0,0 +1,9 @@
+funcionarios = {}
+
+for cont in range(3):
+    cpf=input('informe o cpf do funcionario: ')
+    nome=input('informe o nome do funcionario: ')
+    salario=float(input('informe o salario do funcionario: '))
+
+    funcionarios["cpf"]=cpf
+    funcionarios[]
